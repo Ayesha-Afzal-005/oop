add97789e45b8adfad971a2c47e88bb17c136a5a
Ayesha-Afzal-005 <aayeshaafzal1096@gmail.com>
2025-02-13 16:58:56 +0500

    Add files via upload

---
+++ PrimitiveType.java
@@ -0,0 +1,26 @@
+public class PrimitiveType{
+       public static void main(String args[]){
+             
+             byte b=100;
+             short s=30;   
+             int i=10;
+             long l=400;
+
+             float f=45.0f;
+             double d=90.0;
+
+             boolean bool=true;
+             char ch='A';
+             String st="Ayesha";
+       
+             System.out.printf("Value of int :%d%n", i);
+             System.out.printf("Value of byte :%d%n", b);
+             System.out.printf("Value of short :%d%n", s);
+             System.out.printf("Value of long :%d%n", l);
+             System.out.printf("Value of float :%.2f%n", f);
+             System.out.printf("Value of double :%.2f%n", d);
+             System.out.printf("Value of boolean :%b%n", bool);
+             System.out.printf("Value of char :%c%n", ch);
+             System.out.printf("Value of string :%s%n", st);
+ }
+}
